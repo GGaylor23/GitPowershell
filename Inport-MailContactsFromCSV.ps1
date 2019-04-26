d9e7b3ea7d36c3c5f2aaf3461217132871e7732a
GGaylor23 <50026449+GGaylor23@users.noreply.github.com>
2019-04-26 13:02:30 -0400

    Add files via upload

---
+++ Inport-MailContactsFromCSV.ps1
@@ -0,0 +1,14 @@
+﻿#Import the CSV file
+
+$csvfile = Import-CSV .C:\MSFT-GeGayl\Powershell\Scripts\Contacts.csv
+
+#Loop through CSV file
+
+## Validate that cmdlets are available (verifies EMS/remoting, and RBAC)
+## Create Contact
+## Include error handling, write to console and log (results.log)
+
+foreach ($line in $csvfile) {
+
+New-MailContact -Name $line.name -ExternalEmailAddress $line.ExternalEmailAddress
+}
